9e7fd2545fe94043d2840748d48334c3b80bcc38
Dhruv Tiwari <dhruvdtiwari107@gmail.com>
2024-11-03 18:19:26 +0530

    Exp-7

---
+++ practical_exps/Exp-7.py
@@ -0,0 +1,15 @@
+# 1. Aim: Write a program to demonstrate exception handling in python
+try:
+    a = int(input("Enter the dividend: "))
+    b = int(input("Enter the divisor: "))
+    if b == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        c = a / b
+        print(f"The result of {a} divided by {b} is {c}.")
+        
+except ValueError as e:
+    print("Error: Invalid input. Please enter a valid number.")
+    
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
