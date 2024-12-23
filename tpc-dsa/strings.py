d7cfd0b2bf58707f846715960b2a7aaf2a6905fe
Dhruv Tiwari <dhruvdtiwari107@gmail.com>
2024-12-23 08:40:56 +0530

    ok

---
+++ tpc-dsa/strings.py
@@ -0,0 +1,43 @@
+str1="This is a string.\nwe ae creating it in python"
+#escape sequence character
+print(str1)
+
+
+#concatination
+
+str2="Dhruv"
+l1=len(str2)
+print(l1)
+
+str3="Tiwari"
+l2=len(str3)
+print(l2)
+
+str4=str2+" "+str3
+
+print(str4)
+
+
+
+#indexing in strings
+str="helloword"
+print(str[4])
+
+
+#slicing
+
+
+print(str[1:len(str)])
+
+#slicing negative indexing
+print(str)
+print("Slicing")
+
+
+ok="apple"
+print(ok[-5:-1])
+
+#functions
+
+
+
